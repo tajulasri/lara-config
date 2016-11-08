9b0206f621a6da1927091357f78b12c51ff6476d
Tajul <tajul@nazrol.tech>
2016-11-08 11:58:26 +0800

    update facades binding

--- src/LaraConfig/LaraConfigFacadeServiceProvider.php
+++ src/LaraConfig/LaraConfigFacadeServiceProvider.php
@@ -9,15 +9,6 @@ use Illuminate\Config\Repository as ConfigRepository;
 class LaraConfigFacadeServiceProvider extends ServiceProvider
 {
 
-    protected $cache;
-    protected $config;
-
-    public function __construct(CacheRepository $cache,ConfigRepository $config)
-    {
-        $this->cache = $cache;
-        $this->config = $config;
-    }
-
     /**
      * Bootstrap the application services.
      *
@@ -35,8 +26,6 @@ class LaraConfigFacadeServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('laraconfig.facades',function(){
-            return new LaraConfigRepository($this->cache,$this->config);
-        });
+        $this->app->when('laraconfig.facades')->needs('CacheRepository','ConfigRepository')->give('LaraConfigRepository');
     }
 }
